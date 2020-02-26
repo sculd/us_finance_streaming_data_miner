9784268663258079f7c1cd7f618929d2f0132c71
Jun Lim <hjunlim@google.com>
2020-02-25 19:52:58 -0800

    get_minute_df to Aggregation

--- us_finance_streaming_data_miner/ingest/streaming/aggregation.py
+++ us_finance_streaming_data_miner/ingest/streaming/aggregation.py
@@ -156,6 +156,26 @@ class Aggregation:
         inserted_bar.close = new_bar_with_time.bar.close
         inserted_bar.volume = new_bar_with_time.bar.volume
 
+    def get_minute_df(self, range_minutes = None, print_log = True):
+        if print_log:
+            print('Aggregation.get_minute_df for {symbol}, {l} total bars, range_minutes: {range_minutes}'.format(
+                symbol=self.symbol, l=len(self.bar_with_times), range_minutes=range_minutes if range_minutes else 'all'))
+        tuples = list(map(lambda b: b.to_tuple(), self.bar_with_times))
+        if range_minutes:
+            t_now_tz = self._get_t_now_tz()
+            i = len(tuples)
+            while True:
+                if i == 0:
+                    break
+                else:
+                    dt = t_now_tz - tuples[i - 1][0]
+                    if dt.seconds / 60 >= range_minutes:
+                        break
+                i -= 1
+            tuples = tuples[i:]
+        return pd.DataFrame(tuples, columns = BarWithTime.get_minute_tuple_names())
+
+
 class Aggregations:
     def __init__(self):
         self.aggregation_per_symbol = {}
@@ -180,6 +200,25 @@ class Aggregations:
             l = len(self.aggregation_per_symbol),
             bars_avg = bars_avg
         )
+
+    def get_minute_df(self, print_log = True):
+        if print_log:
+            logging.info('Aggregations.get_minute_df for {l_s} symbols'.format(l_s=len(self.aggregation_per_symbol)))
+        df = pd.DataFrame(columns=BarWithTime.get_minute_tuple_names())
+        for symbol, aggregation in self.aggregation_per_symbol.items():
+            t_1 = datetime.datetime.utcnow()
+            df_ = aggregation.get_minute_df()
+            t_2 = datetime.datetime.utcnow()
+            dt_21 = t_2 - t_1
+            logging.info('{s} seconds {ms} microseconds took to get minute_df for symbol {symbol}'.format(
+                s=dt_21.seconds, ms=dt_21.microseconds, symbol=symbol))
+            df = df.append(df_)
+            t_3 = datetime.datetime.utcnow()
+            dt_32 = t_3 - t_2
+            logging.info('{s} seconds {ms} microseconds took to append for symbol {symbol}'.format(
+                s=dt_32.seconds, ms=dt_32.microseconds, symbol=symbol))
+        return df.set_index('datetime')
+
 
 class AggregationsRun:
     def __init__(self, aggregations = None):
--- us_finance_streaming_data_miner/ingest/streaming/daily_aggregation.py
+++ us_finance_streaming_data_miner/ingest/streaming/daily_aggregation.py
@@ -5,25 +5,6 @@ from us_finance_streaming_data_miner.ingest.streaming.aggregation import BarWith
 
 
 class DailyAggregation(Aggregation):
-    def get_minute_df(self, range_minutes = None, print_log = True):
-        if print_log:
-            print('Aggregation.get_minute_df for {symbol}, {l} total bars, range_minutes: {range_minutes}'.format(
-                symbol=self.symbol, l=len(self.bar_with_times), range_minutes=range_minutes if range_minutes else 'all'))
-        tuples = list(map(lambda b: b.to_tuple(), self.bar_with_times))
-        if range_minutes:
-            t_now_tz = self._get_t_now_tz()
-            i = len(tuples)
-            while True:
-                if i == 0:
-                    break
-                else:
-                    dt = t_now_tz - tuples[i - 1][0]
-                    if dt.seconds / 60 >= range_minutes:
-                        break
-                i -= 1
-            tuples = tuples[i:]
-        return pd.DataFrame(tuples, columns = BarWithTime.get_minute_tuple_names())
-
     def get_daily_df(self):
         df_minute = self.get_minute_df()
         print('Aggregation.get_daily_df for {symbol}, df_minute length: {l}'.format(symbol=self.symbol, l=len(df_minute )))
@@ -40,24 +21,6 @@ class DailyAggregation(Aggregation):
         return df_daily
 
 class DailyAggregations(Aggregations):
-    def get_minute_df(self, print_log = True):
-        if print_log:
-            logging.info('Aggregations.get_minute_df for {l_s} symbols'.format(l_s=len(self.aggregation_per_symbol)))
-        df = pd.DataFrame(columns=BarWithTime.get_minute_tuple_names())
-        for symbol, aggregation in self.aggregation_per_symbol.items():
-            t_1 = datetime.datetime.utcnow()
-            df_ = aggregation.get_minute_df()
-            t_2 = datetime.datetime.utcnow()
-            dt_21 = t_2 - t_1
-            logging.info('{s} seconds {ms} microseconds took to get minute_df for symbol {symbol}'.format(
-                s=dt_21.seconds, ms=dt_21.microseconds, symbol=symbol))
-            df = df.append(df_)
-            t_3 = datetime.datetime.utcnow()
-            dt_32 = t_3 - t_2
-            logging.info('{s} seconds {ms} microseconds took to append for symbol {symbol}'.format(
-                s=dt_32.seconds, ms=dt_32.microseconds, symbol=symbol))
-        return df.set_index('datetime')
-
     def get_daily_df(self):
         logging.info('Aggregations.get_daily_df for {l_s} symbols'.format(l_s=len(self.aggregation_per_symbol)))
         df = pd.DataFrame(columns=BarWithTime.get_daily_tuple_names())
